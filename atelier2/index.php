07009c1fe007ddb50c4676062c3b27346dd955c4
HyperCops <anthonydealmeidamajor@gmail.com>
2025-11-05 10:36:09 +0100

    index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
         $mdpgen === bin2hex(random_bytes(16))
-        setcookie('authToken', mdpgen, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        setcookie('authToken', $mdpgen, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
